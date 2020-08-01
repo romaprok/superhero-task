32774d87582f83ba250c50b45e2063b0f6ce8977
PRoman93 <roman_prokopets@mail.ua>
2020-08-01 09:33:03 +0300

    first commit

---
+++ src/ui/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>nickName:</span>
+      <span>real_name:</span>
+      <span>origin_description:</span>
+      <span>superpowers:</span>
+      <span>catch_phrase:​</span>
+      <span>Images:</span>
+    </div>
+  );
+}
+
+export default App;
